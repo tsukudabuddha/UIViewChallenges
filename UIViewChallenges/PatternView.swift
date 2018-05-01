863de37d4875904552ad278c2ebdb05eee308cdd
tsukudabuddha <andrewtsukuda@gmail.com>
2018-05-01 15:50:12 -0700

    Added gitignore

---
+++ UIViewChallenges/PatternView.swift
@@ -0,0 +1,21 @@
+//
+//  PatternView.swift
+//  UIViewChallenges
+//
+//  Created by Andrew Tsukuda on 5/1/18.
+//  Copyright © 2018 Andrew Tsukuda. All rights reserved.
+//
+
+import UIKit
+
+class PatternView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
